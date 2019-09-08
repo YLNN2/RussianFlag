6f4840b35ed2f72fc273bde755f7b28f424b3d45
flagshtock <lguseva87@gmail.com>
2019-09-08 19:05:13 +0300

    Нижний сегмент красного цвета.

--- first.py
+++ first.py
@@ -25,10 +25,16 @@ def blue_rect():
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
 
 
+def red_rect():
+    rect_color = pygame.Color('red')
+    rect_width = 0
+    rect_point = [(30, 250), (300, 100)]
+    pygame.draw.rect(screen, rect_color, rect_point, rect_width)
+
 flag()
 white_rect()
 blue_rect()
-# слияние белого и синего
+red_rect()
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
